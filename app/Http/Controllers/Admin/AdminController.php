a8274888d11a5187ab82a32b9562ee916f4726e4
TheMartes <matt.buncek@gmail.com>
2017-11-02 21:07:23 +0100

    Application Settings logic was added

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Settings;
+use App\Http\Requests\UpdateSettings;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    public function settings()
+    {
+        $settings = Settings::get();
+        return view('admin.settings')->with('settings', $settings);
+    }
+
+    public function settingsUpdate(Request $request)
+    {
+        $options = Settings::get();
+
+        for($id = 1; $id <= count($options); $id++)
+        {
+            $option = Settings::findOrFail($id);
+            $option->value = request($option->key); # @TODO
+            $option->save();
+        }
+
+        session()->flash('success', 'Settings were updated');
+        return redirect('admin/settings');
+    }
+
+}
